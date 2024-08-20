3e91942e8d5d2f753709556885eae3c4ff1f18e1
Kiran Yallabandi <kiranyallabandi@gmail.com>
2024-08-21 02:07:37 +0530

    preparing to initialise the site as an angular webapp

--- src/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event)=> {
-    const placeholder = document.querySelector('#placeholder');
-    placeholder.innerText = "Hello from placeholder!";
-});
